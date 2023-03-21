99cad373c8f66f95e30b6718b56fa34919dff679
Liendson Douglas <liendsondouglas1@gmail.com>
2023-03-21 09:42:35 -0300

    Adicionando componente dinâmico

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,5 +8,16 @@
       <v-card-subtitle>Subtítulo</v-card-subtitle>
       <v-card-text>Conteudo do Card</v-card-text>
     </v-card>
+    <teste-component>
+      <div slot="header">
+        <h1>Aqui pode estar um título da página</h1>
+      </div>
+      <div slot="footer">
+        <h2>Aqui pode estar um rodapé da página</h2>
+      </div>
+      <div>
+        <h1>Aqui é o conteudo</h1>
+      </div>
+    </teste-component>
   </div>
 </template>
